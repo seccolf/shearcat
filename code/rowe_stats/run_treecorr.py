4122e63ab097311b86ab5026d573c863af45e112
Lucas Secco <secco@uchicago.edu>
2022-04-11 14:37:10 -0500

    some runfiles used

--- code/rowe_stats/run_treecorr.py
+++ code/rowe_stats/run_treecorr.py
@@ -6,21 +6,22 @@ from os import listdir,environ
 ###############
 #this script assumes you'll star 4 slurm tasks(processes), each of which may have many CPUs
 #and each of which will run treecorr on a single band
+#but if you run with less than 4 processes, you'll only get some (but not all) of the bands (see band_dict below)
 ###############
 
 #treecorr setup:
 ############
-bslop= 3.0
+bslop= 0.01
 min_angle = 0.1 #arcmin
 max_angle = 250.0 #arcmin
 nbins = 25
 ############
 
 #organize processes in nodes
-PROCESS = int(environ('SLURM_PROCID')) 
-band_dict = {1:'g', 2:'r', 3:'i', 4:'z'}
+PROCESS = int(environ['SLURM_PROCID']) 
+band_dict = {0:'g', 1:'r', 2:'i', 3:'z'}
 band = band_dict[PROCESS]
-print('PROCESS %d is running the %s-band'%(PROCESS,band))
+print('PROCESS %d is running the %s-band'%(PROCESS,band),flush=True)
 
 #find files
 location_of_exposures = '/home/secco/project2-kicp-secco/delve/rowe_stats_measurements/'+band+'/'
@@ -44,7 +45,7 @@ for expame in all_exposures:
 	g2_star = np.append(g2_star, f[1].data['g2_star'])
 	g2_model = np.append(g2_model, f[1].data['g2_model'])
 
-print('Total number of objects for %d exposures in band %s: %d'%(len(all_exposures),band,len(ra)))
+print('Total number of objects for %d exposures in band %s: %d'%(len(all_exposures),band,len(ra)),flush=True)
 
 #now create the residual vector and pass everything to treecorr
 q1 = g1_star-g1_model
@@ -56,10 +57,10 @@ cat_e = treecorr.Catalog(g1=g1_model, g2=g2_model, ra=ra, dec=dec, ra_units='deg
 GG = treecorr.GGCorrelation(nbins=nbins,min_sep=min_angle,max_sep=max_angle,sep_units='arcmin',verbose=3,bin_slop=bslop)
 GG.process(cat_e)
 GG.write(location_of_output+'rho0_%sband_bslop%1.3f_%dexposures.txt'%(band, bslop,len(all_exposures)))
-print('Done rho0 in band %s'%band)
+print('Done rho0 in band %s'%band,flush=True)
 GG.process(cat_q)
 GG.write(location_of_output+'rho1_%sband_bslop%1.3f_%dexposures.txt'%(band, bslop,len(all_exposures)))
-print('Done rho1 in band %s'%band)
+print('Done rho1 in band %s'%band,flush=True)
 GG.process(cat_e,cat_q)
 GG.write(location_of_output+'rho2_%sband_bslop%1.3f_%dexposures.txt'%(band, bslop,len(all_exposures)))
-print('Done rho2 in band %s'%band)
+print('Done rho2 in band %s'%band,flush=True)
