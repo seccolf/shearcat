4698cb10b450c826a5312a15ac5faca3433ebe55
Lucas Secco <secco.lf@gmail.com>
2022-04-12 12:33:16 -0500

    runfile for combine

---
+++ code/rowe_stats/run_combine.sh
@@ -0,0 +1,13 @@
+#!/bin/bash                                                                                                                                                    
+
+echo "RUNNING"
+echo "slurm procid = " $SLURM_PROCID
+echo "slurm ntasks = " $SLURM_NTASKS
+echo "slurm cpus per task = " $SLURM_CPUS_PER_TASK
+
+#Compute 3pt correlation measurement                                                                                                                           
+cmd="python ./combine_outputs.py"
+date
+echo $cmd
+$cmd
+date
