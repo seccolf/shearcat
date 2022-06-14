dc4261b5d3833743dc4e62bf536af74a679ee60e
Lucas Secco <secco.lf@gmail.com>
2022-06-14 11:44:39 -0500

    quick push to run something on midway

--- code/rowe_stats/measure_stars_models.py
+++ code/rowe_stats/measure_stars_models.py
@@ -562,7 +562,7 @@ for expname in exps_for_this_process: #loops over exposures!
             imaflags_iso_out,flux_auto_out,fluxerr_auto_out,flux_aper_8_out,fluxerr_aper_8_out,
             N_failed_stars,N_failed_CCDS,N_bad_match)
     time2 = time()
-    time_it_took = time2-time1
+    time_it_took = (time2-time1)/60.0
     print('PROCESS %d DONE: wrote %s to  %s (took %1.2f minutes)'%(PROCESS,expname,outputfile_name,time_it_took),flush=True)
 
     track_whats_done = open(output_location+'DONE_EXPS.txt','a')
